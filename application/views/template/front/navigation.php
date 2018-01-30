9a7dab371f0a01147ede958dae8be8edbf483f19
detautama <detautama11@gmail.com>
2018-01-30 23:01:01 +0800

    Update Categories & Search Options

--- application/views/template/front/navigation.php
+++ application/views/template/front/navigation.php
@@ -1,56 +1,103 @@
 <!-- CSS dropdown login -->
 <style media="screen">
-#login-dp{
-  min-width: 250px;
-  padding: 14px 14px 0;
-  overflow:hidden;
-  background-color:rgba(255,255,255,.8);
-}
-#login-dp .help-block{
-  font-size:12px
-}
-#login-dp .bottom{
-  background-color:rgba(255,255,255,.8);
-  border-top:1px solid #ddd;
-  clear:both;
-  padding:14px;
-}
-#login-dp .social-buttons{
-  margin:12px 0
-}
-#login-dp .social-buttons a{
-  width: 49%;
-}
-#login-dp .form-group {
-  margin-bottom: 10px;
-}
-.btn-fb{
-  color: #fff;
-  background-color:#3b5998;
-}
-.btn-fb:hover{
-  color: #fff;
-  background-color:#496ebc
-}
-.btn-tw{
-  color: #fff;
-  background-color:#55acee;
-}
-.btn-tw:hover{
-  color: #fff;
-  background-color:#59b5fa;
-}
-@media(max-width:768px){
-  #login-dp{
-    background-color: inherit;
+  #login-dp {
+    min-width: 250px;
+    padding: 14px 14px 0;
+    overflow: hidden;
+    background-color: rgba(255, 255, 255, .8);
+  }
+
+  #login-dp .help-block {
+    font-size: 12px
+  }
+
+  #login-dp .bottom {
+    background-color: rgba(255, 255, 255, .8);
+    border-top: 1px solid #ddd;
+    clear: both;
+    padding: 14px;
+  }
+
+  #login-dp .social-buttons {
+    margin: 12px 0
+  }
+
+  #login-dp .social-buttons a {
+    width: 49%;
+  }
+
+  #login-dp .form-group {
+    margin-bottom: 10px;
+  }
+
+  .btn-fb {
     color: #fff;
+    background-color: #3b5998;
   }
-  #login-dp .bottom{
-    background-color: inherit;
-    border-top:0 none;
+
+  .btn-fb:hover {
+    color: #fff;
+    background-color: #496ebc
   }
-}
+
+  .btn-tw {
+    color: #fff;
+    background-color: #55acee;
+  }
+
+  .btn-tw:hover {
+    color: #fff;
+    background-color: #59b5fa;
+  }
+
+  .tes{
+    top: -3px;
+    left: 100%;
+  }
+
+  .tes a{
+    margin-left: 0px;
+    
+  }
+
+  select{
+    height: 45px;
+    margin: 0;
+    background-color: #ff6e00;
+    color: white;
+    border: none;
+  }
+
+  select option{
+    background-color: #fff;
+    color: #000;
+  }
+
+
+  @media(max-width:768px) {
+    #login-dp {
+      background-color: inherit;
+      color: #fff;
+    }
+    #login-dp .bottom {
+      background-color: inherit;
+      border-top: 0 none;
+    }
+    .tes a{
+      margin-left: 30px;
+      font-style: italic;
+    }
+  }
 </style>
+<script>
+  $(document).ready(function () {
+    $('.dropdown-submenu a.test').on("click", function (e) {
+      $(this).next('ul').toggle();
+      e.stopPropagation();
+      e.preventDefault();
+    });
+  });
+</script>
 <nav class="navbar navbar-default navbar-fixed-top">
   <div class="container">
     <div class="navbar-header">
@@ -63,102 +110,102 @@
     </div>
     <div class="collapse navbar-collapse" id="myNavbar">
       <ul class="nav navbar-nav">
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Ring
-            <span class="caret"></span>
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-              <a href="#">Page 1-1</a>
-            </li>
-            <li>
-              <a href="#">Page 1-2</a>
-            </li>
-            <li>
-              <a href="#">Page 1-3</a>
-            </li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Necklace
-            <span class="caret"></span>
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-              <a href="#">Page 1-1</a>
-            </li>
-            <li>
-              <a href="#">Page 1-2</a>
-            </li>
-            <li>
-              <a href="#">Page 1-3</a>
-            </li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Bracelet
-            <span class="caret"></span>
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-              <a href="#">Page 1-1</a>
-            </li>
-            <li>
-              <a href="#">Page 1-2</a>
-            </li>
-            <li>
-              <a href="#">Page 1-3</a>
-            </li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Earring
-            <span class="caret"></span>
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-              <a href="#">Page 1-1</a>
-            </li>
-            <li>
-              <a href="#">Page 1-2</a>
-            </li>
-            <li>
-              <a href="#">Page 1-3</a>
-            </li>
-          </ul>
+        <li class="active">
+          <div class="dropdown">
+            <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" style="margin-top: 5px;">Categories
+              <span class="caret"></span>
+            </button>
+            <ul class="dropdown-menu">
+              <li class="dropdown-submenu">
+                <a class="test" tabindex="-1" href="#">Ring
+                  <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu tes">
+                  <li>
+                    <a tabindex="-1" href="#">Ring blue</a>
+                  </li>
+                  <li>
+                    <a tabindex="-1" href="#">Ring Red</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a class="test" tabindex="-1" href="#">Necklace
+                  <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu tes">
+                  <li>
+                    <a tabindex="-1" href="#">Necklace Red</a>
+                  </li>
+                  <li>
+                    <a tabindex="-1" href="#">Necklace Yellow</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a class="test" tabindex="-1" href="#">Bracelet
+                  <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu tes">
+                  <li>
+                    <a tabindex="-1" href="#">Bracelet Red</a>
+                  </li>
+                  <li>
+                    <a tabindex="-1" href="#">Bracelet Yellow</a>
+                  </li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a class="test" tabindex="-1" href="#">Earing
+                  <span class="caret"></span>
+                </a>
+                <ul class="dropdown-menu tes">
+                  <li>
+                    <a tabindex="-1" href="#">Earing Red</a>
+                  </li>
+                  <li>
+                    <a tabindex="-1" href="#">Earing Red</a>
+                  </li>
+                  <li>
+                    <a tabindex="-1" href="#">Earing Red</a>
+                  </li>
+                  <li>
+                    <a tabindex="-1" href="#">Earing Yellow</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </li>
       </ul>
       <form class="navbar-form navbar-left" method="get" action="<?php echo base_url().'index.php/Product/public_product_list_view'; ?>">
         <div class="form-group">
-          <input type="text" name="search_value" value="<?php echo $search_value = (isset($search_value)) ? $search_value : "" ; ?>" class="form-control" placeholder="Search">
+          <input style="margin-bottom: 3px;" type="text" name="search_value" value="<?php echo $search_value = (isset($search_value)) ? $search_value : "
+            " ; ?>" class="form-control" placeholder="Search">
         </div>
-        <div class="btn-group">
-          <a class="btn btn-warning dropdown-toggle" data-toggle="dropdown" href="#">Seller
-            <span class="caret"></span>
-          </a>
-          <ul class="dropdown-menu">
-            <li>
-              <a href="#">Product</a>
-            </li>
-          </ul>
-        </div>
+        <select>
+            <option value="volvo">Seller</option>
+            <option value="saab">Product</option>
+        </select>
       </form>
       <ul class="nav navbar-nav navbar-right">
         <li>
           <a href="#">
             <span class="glyphicon glyphicon-user"></span> Sign Up</a>
-          </li>
+        </li>
 
-          <!--  -->
+        <!--  -->
 
         <li class="dropdown">
           <?php if (empty($this->session->userdata('id_buyer'))): ?>
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown">  <span class="glyphicon glyphicon-log-in"></span> Login</a>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <span class="glyphicon glyphicon-log-in"></span> Login</a>
           <ul id="login-dp" class="dropdown-menu">
             <li>
               <div class="row">
                 <div class="col-md-12">
-                  <form class="form" role="form" method="post" action="<?php echo base_url().'index.php/Login/login';?>" accept-charset="UTF-8" id="login-nav">
+                  <form class="form" role="form" method="post" action="<?php echo base_url().'index.php/Login/login';?>" accept-charset="UTF-8"
+                    id="login-nav">
                     <div class="form-group">
                       <label class="sr-only" for="exampleInputEmail2">Email address</label>
                       <input type="email" name="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
@@ -175,24 +222,27 @@
                 <!-- <div class="bottom text-center">
                 New here ? <a href="#"><b>Join Us</b></a>
               </div> -->
-            </div>
+              </div>
             </li>
-            </ul>
+          </ul>
           <?php else: ?>
-              <a href="#"  class="dropdown-toggle" data-toggle="dropdown" ><span class=""></span><?php echo $this->session->userdata('first_name'); ?></a>
-              <ul class="dropdown-menu">
-                <li>
-                  <a href="<?php echo base_url().'index.php/Buyer/buyer_account_view';?>">Profile</a>
-                </li>
-                <li>
-                  <a href="<?php echo base_url().'index.php/Quotation/buyer_quotation_list';?>">Request for Quotation List</a>
-                </li>
-                <li>
-                  <a href="<?php echo base_url().'index.php/Login/logout';?>">Sign Out</a>
-                </li>
-              </ul>
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+            <span class=""></span>
+            <?php echo $this->session->userdata('first_name'); ?>
+          </a>
+          <ul class="dropdown-menu">
+            <li>
+              <a href="<?php echo base_url().'index.php/Buyer/buyer_account_view';?>">Profile</a>
+            </li>
+            <li>
+              <a href="<?php echo base_url().'index.php/Quotation/buyer_quotation_list';?>">Request for Quotation List</a>
+            </li>
+            <li>
+              <a href="<?php echo base_url().'index.php/Login/logout';?>">Sign Out</a>
+            </li>
+          </ul>
           <?php endif; ?>
-          </li>
+        </li>
 
 
 
@@ -201,8 +251,8 @@
 
 
 
-          <!--  -->
+        <!--  -->
 
-      </div>
     </div>
-  </nav>
+  </div>
+</nav>
