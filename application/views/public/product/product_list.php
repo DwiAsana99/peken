41c07ceea7b60cb53cb0f271cf6410b9e409ada5
detautama <detautama11@gmail.com>
2018-02-01 20:38:16 +0800

    Feature Added

--- application/views/public/product/product_list.php
+++ application/views/public/product/product_list.php
@@ -10,12 +10,14 @@
     </ol>
     <div class="my-container">
       <?php $i = 1; foreach($product as $p){ ?>
-      <div>
+      <div class="tes-hover">
         <img class="img-responsive" src="<?php echo base_url('assets/supplier_upload/').$p->FileName?>" alt="">
-        <h4>IDR Rp1.200.000 / pcs</h4>
-        <h5>Kesslers Diamonds</h5>
-        <h6>10 pcs (Supply Ability)</h6>
-        <h6>Daily (Period Ability)</h6>
+        <div class="detail-display">
+          <h4>IDR Rp1.200.000 / pcs</h4>
+          <h5>Kesslers Diamonds</h5>
+          <h6>10 pcs (Supply Ability)</h6>
+          <h6>Daily (Period Ability)</h6>
+        </div>
         <hr>
         <div class="text-center">
           <h6>Art Silver</h6>
