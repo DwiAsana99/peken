145909529e270bcd17e6ca849b9d2a1688106dd3
detautama <detautama11@gmail.com>
2018-02-03 13:24:24 +0800

    Datatable updated

--- application/views/private/quotation/buyer_quotation_list.php
+++ application/views/private/quotation/buyer_quotation_list.php
@@ -1,65 +1,75 @@
+<style>
+    table a:not(.btn),
+    .table a:not(.btn) {
+        text-decoration: none;
+    }
 
-    <style>
-        table a:not(.btn),
-        .table a:not(.btn) {
-            text-decoration: none;
-        }
+    tr.hover {
+        cursor: pointer;
+        /* whatever other hover styles you want */
+    }
 
-        tr.hover {
-            cursor: pointer;
-            /* whatever other hover styles you want */
-        }
-    </style>
+    #example {
+        display: block;
+        overflow-x: auto;
+        white-space: nowrap;
+    }
+</style>
 
-    <div class="container">
-        <h1>Quotation List</h1>
-        <ol class="breadcrumb">
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a>Quotation List</a>
-            </li>
-        </ol>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
 
-        <input type="text" id="myInput" onkeyup="myFunction()" placeholder="Search for.." title="Type in a name">
+<script src="https://cdn.datatables.net/1.10.16/js/dataTables.bootstrap.min.js"></script>
 
-        <table id="myTable">
-          <?php foreach($quotation as $q){ ?>
+<div class="container">
+    <h1>Quotation List</h1>
+    <ol class="breadcrumb">
+        <li>
+            <a href="#">Home</a>
+        </li>
+        <li>
+            <a>Quotation List</a>
+        </li>
+    </ol>
+
+    <table id="example" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
             <tr>
+                <th>Send</th>
+                <th>Subject</th>
+                <th>Message</th>
+                <th>Date</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach($quotation as $q){ ?>
+            <tr class="click">
                 <td>
-                    <a href="<?php echo base_url().'index.php/Quotation/buyer_quotation_detail?id_quotation='.$q->IdQuotation; ?>">To: <?php echo $q->CompanyName  ?></a>
+                    <a href="<?php echo base_url().'index.php/Quotation/buyer_quotation_detail?id_quotation='.$q->IdQuotation; ?>">To:
+                        <?php echo $q->CompanyName  ?>
+                    </a>
                 </td>
-                <td>Pembelian <?php echo $q->Name  ?></td>
-                <td><?php echo trim(substr($q->Content,0,50))." <b>...</b>" ?></td>
-                <td><?php echo $q->DateSend  ?></td>
+                <td>Pembelian
+                    <?php echo $q->Name  ?>
+                </td>
+                <td>
+                    <?php echo trim(substr($q->Content,0,50))." <b>...</b>" ?>
+                </td>
+                <td>
+                    <?php echo $q->DateSend  ?>
+                </td>
             </tr>
             <?php } ?>
+        </tbody>
+    </table>
+</div>
 
-        </table>
-    </div>
-    <script>
-        function myFunction() {
-            var input, filter, table, tr, td, i;
-            input = document.getElementById("myInput");
-            filter = input.value.toUpperCase();
-            table = document.getElementById("myTable");
-            tr = table.getElementsByTagName("tr");
-            for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td")[0];
-                if (td) {
-                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                        tr[i].style.display = "";
-                    } else {
-                        tr[i].style.display = "none";
-                    }
-                }
-            }
-        }
-
-        $('tr').click(function () {
+<script>
+    $(document).ready(function () {
+        $('#example').DataTable();
+        $('.click').click(function () {
             window.location = $(this).find('a').attr('href');
         }).hover(function () {
             $(this).toggleClass('hover');
         });
-    </script>
+    });
+</script>
--- application/views/template/front/css_front.php
+++ application/views/template/front/css_front.php
@@ -1,4 +1,5 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.7/cosmo/bootstrap.min.css">
+<link rel="stylesheet" href="https://cdn.datatables.net/1.10.16/css/dataTables.bootstrap.min.css">
 <link rel="stylesheet" href="<?php echo base_url('assets/front_end_assets/bower_components/font-awesome/css/font-awesome.min.css') ?>" />
 <link rel="stylesheet" href="<?php echo base_url('assets/front_end_assets/css/master.css') ?>" />
 <link rel="stylesheet" href="<?php echo base_url('assets/front_end_assets/css/index.css') ?>" />
