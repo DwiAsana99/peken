a77e259d655203dcb31f0cde2cf97f1b3cf5929e
PREMAWAISNAWA <premawaisnawa@gmail.com>
2018-02-13 20:08:18 +0800

    setingan sama dengan hosttingan

--- application/config/email.php
+++ application/config/email.php
@@ -2,16 +2,17 @@
 
 $config = array(
   'protocol' => 'smtp',
-  'smtp_host' => 'smtp-mail.outlook.com',
+  'smtp_host' => 'smtp.gmail.com',
   'smtp_port' => 587,
-  'smtp_user' => "premawaisnawa@outlook.com",
+  'smtp_user' => "marketplacesilver@gmail.com",
   'smtp_pass' => "34GVvbl97Gpq",
   'mailtype' => "html",
   'charset' => 'iso-8859-1',
   'crlf' => "\r\n",
   'newline' => "\r\n",
   'validate' => TRUE,
-  'wordwrap' => TRUE
+  'wordwrap' => TRUE,
+  'smtp_crypto' => 'ssl'
 );
 //smtp.live.com  'smtp_crypto' => 'ssl'
 //smtp-mail.outlook.com
