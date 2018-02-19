6b22d0d71322c3e602443bc97183c85868df3ebe
PREMAWAISNAWA <premawaisnawa@gmail.com>
2018-02-19 22:18:32 +0800

    Pagination product list complete

--- application/models/M_pagination.php
+++ application/models/M_pagination.php
@@ -27,10 +27,10 @@ class M_pagination extends CI_Model{
     $this->page_query_string = !empty($page_query_string) ? $page_query_string : TRUE ;
     if(!empty($per_page)){$this->per_page = $per_page;}
     $this->use_page_numbers = !empty($use_page_numbers) ? $use_page_numbers : TRUE ;
-    $this->cur_tag_open = !empty($cur_tag_open) ?  $cur_tag_open : '&nbsp;<span>';
-    $this->cur_tag_close = !empty($cur_tag_close) ? $cur_tag_close : '</span>' ;
+    $this->cur_tag_open = !empty($cur_tag_open) ?  $cur_tag_open : '&nbsp;<li class="active"><span>';
+    $this->cur_tag_close = !empty($cur_tag_close) ? $cur_tag_close : '</span></li>' ;
     $this->next_link = !empty($next_link) ? $next_link : 'Next' ;
-    $this->prev_link = !empty($prev_link) ? $prev_link : 'Previous' ;
+    $this->prev_link = !empty($prev_link) ? $prev_link : 'Prev' ;
     if(!empty($base_url)){$this->base_url = base_url().$base_url;}
     if(!empty($num_rows)){
       $this->total_rows = $num_rows;
@@ -38,6 +38,22 @@ class M_pagination extends CI_Model{
     }
   }
   function get_config(){
+    $config['num_tag_open'] = '<li class="hidden-xs">';
+    $config['num_tag_close'] = '</li>';
+    // $config['full_tag_open'] = '<li><span>';
+    // $config['full_tag_close'] = '</span></li>';
+    $config['first_link'] = 'First Page';
+    $config['first_tag_open'] = '<li class="hidden-xs"><span>';
+    $config['first_tag_close'] = '</span></li>';
+    $config['last_link'] = 'Last Page';
+    $config['last_tag_open'] = '<li class="hidden-xs"><span>';
+    $config['last_tag_close'] = '</span></li>';
+
+    $config['next_tag_open'] = '<li><span>';
+    $config['next_tag_close'] = '</span></li>';
+
+    $config['prev_tag_open'] = '<li><span>';
+    $config['prev_tag_close'] = '</span></li>';
     $config['page_query_string'] = $this->page_query_string;
     $config["per_page"] = $this->per_page;
     $config['use_page_numbers'] = $this->use_page_numbers;
@@ -47,7 +63,7 @@ class M_pagination extends CI_Model{
     $config['prev_link'] = $this->prev_link;
     $config["base_url"] =$this->base_url;
     $config["total_rows"] =$this->total_rows;
-    $config['num_links'] =$this->num_links;
+    $config['num_links'] =1;//$this->num_links;
     return $config;
   }
   function get_offset($page="") {
