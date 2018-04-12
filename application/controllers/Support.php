7d9d7a3f57b231272c3657bd9d770695c2f7e6aa
PREMAWAISNAWA <premawaisnawa@gmail.com>
2018-04-12 15:59:47 +0800

    baru sampai di add support detail dan nungkak

--- application/controllers/Support.php
+++ application/controllers/Support.php
@@ -8,7 +8,7 @@ class Support extends CI_Controller{
 		parent::__construct();
 		$this->load->library(array('form_validation','pagination'));
 		$this->load->helper(array('form', 'url'));
-		$this->load->model(array('M_product','M_product_category','M_product_sub_category','M_pagination','M_quotation','M_quotation_detail','M_support'));
+		$this->load->model(array('M_product','M_product_category','M_product_sub_category','M_pagination','M_quotation','M_quotation_detail','M_support','M_support_detail','M_date'));
 	}
   function buyer_support_list_view(){
     $get_product_category = $this->M_product_category->get_product_category();
@@ -48,7 +48,33 @@ class Support extends CI_Controller{
     $this->load->view('private/support/member_support_list');
     $this->load->view('template/back_admin/admin_foot');
   }
-  function get_buyer_support_json(){
+  function buyer_support_detail(){
+    $id_buyer = $this->session->userdata('id_buyer');
+    $get_product_category = $this->M_product_category->get_product_category();
+    $get_product_sub_category = $this->M_product_sub_category->get_product_sub_category_all();
+    $data_nav['product_category'] = $get_product_category->result();
+    $data_nav['product_sub_category'] = $get_product_sub_category->result();
+    if ($this->session->userdata('id_buyer')) {
+      $id_buyer = $this->session->userdata('id_buyer');
+      $get_unread_qutation_detail = $this->M_quotation_detail->get_unread_qutation_detail("",$id_buyer);
+      $data_nav['unread_quotation_detail'] = $get_unread_qutation_detail->result();
+      $data_nav['unread_quotation_detail_num_rows'] = $get_unread_qutation_detail->num_rows();
+    }
+    $support_code = $this->input->get('support_code');
+    $filter_value = array('support_code' => $support_code);
+    $get_support = $this->M_support->get_support($filter_value);
+    $get_support_detail = $this->M_support_detail->get_support_detail($filter_value, 'DateSend ASC');
+    $data['support'] = $get_support->result();
+    $data['support_detail'] = $get_support_detail->result();
+    //print_r($data['support_detail']);exit();
+    $head_data['page_title'] = "Dinilaku";
+    $this->load->view('template/front/head_front',$head_data);
+    $this->load->view('template/front/navigation',$data_nav);
+    $this->load->view('private/support/buyer_support_detail',$data);
+    $this->load->view('template/front/foot_front');
+  }
+  function get_buyer_support_json()
+  {
     $id_buyer = $this->session->userdata('id_buyer');
     $filter_value = array('id_member' => $id_buyer);
     $get_support = $this->M_support->get_support($filter_value, 'DateSend DESC');
@@ -62,10 +88,10 @@ class Support extends CI_Controller{
       "Subject" => $bar->Subject,
       "DateSend" => $bar->DateSend,
       "IsClosed" => $is_closed,
-      "DetailButton" => '<a class="btn btn-info" href="'.base_url("index.php/Product_category/product_category_edit_view/").$bar->SupportCode.'">
-      <span class="fa fa-fw fa-eye" >
+      "DetailButton" => "<a class='btn btn-info' href=".base_url('index.php/Support/buyer_support_detail?support_code='.$bar->SupportCode).">
+      <span class='fa fa-fw fa-eye' >
       </span>
-      </a>'
+      </a>"
       );
       $data[] = $row;
     }
@@ -77,6 +103,7 @@ class Support extends CI_Controller{
     );
     echo json_encode($output);
    }
+
    function get_supplier_support_json(){
      $id_supplier = $this->session->userdata('id_supplier');
      $filter_value = array('id_member' => $id_supplier);
@@ -106,6 +133,7 @@ class Support extends CI_Controller{
      );
      echo json_encode($output);
     }
+
     function get_member_support_json(){
       // $id_supplier = $this->session->userdata('id_supplier');
       // $filter_value = array('id_member' => $id_supplier);
@@ -137,6 +165,47 @@ class Support extends CI_Controller{
         "data" => $data
       );
       echo json_encode($output);
+     }
+     function add_support_detail(){
+       $support_code = $this->input->post('support_code');
+       $id_member = $this->input->post('id_member');
+       $message = $this->input->post('message');
+       $date = $this->M_date->get_date_sql_format();
+       $data = array(
+         'SupportCode' => $support_code,
+         'IdMember' => $id_member,
+         'Message' => $message,
+         'DateSend' => $date
+       );
+       $id_quotation_detail = $this->M_quotation_detail->add_quotation_detail($data);
+       $get_quotation_detail = $this->M_quotation_detail->get_quotation_detail($id_quotation,$id_quotation_detail);
+       $quotation_detail = $get_quotation_detail->row();
+       $profile_image = $quotation_detail->ProfilImage;
+       $profile_image = !empty($profile_image) ? $profile_image : "user_without_profile_image.png" ;
+       echo '<li class="right clearfix"><span class="chat-img pull-right">
+         <img src='.base_url('assets/supplier_upload/').$profile_image.' alt="User Avatar" width="60" class="img-circle" />
+       </span>
+       <div class="chat-body clearfix">
+         <div class="header">
+           <small class=" text-muted"><span class="glyphicon glyphicon-time"></span>'.$date.'</small>
+           <strong class="pull-right primary-font">'."Me".'</strong>
+         </div>
+         <p>
+           '.$quotation_detail->Message.'
+         </p>
+       </div>
+     </li>';
+       // echo '<li class="left clearfix"><span class="chat-img pull-left">
+       //   <img src='.base_url('assets/supplier_upload/').$quotation_detail->ProfilImage.' alt="User Avatar" class="img-circle" />
+       // </span>
+       // <div class="chat-body clearfix">
+       //   <div class="header">
+       //     <strong class="primary-font">'.$quotation_detail->CompanyName.'</strong> <small class="pull-right text-muted">
+       //       <span class="glyphicon glyphicon-time"></span>'.$date.'</small>
+       //     </div>
+       //     <p>'.$quotation_detail->Message.'</p>
+       //   </div>
+       // </li>';
      }
 }
 
