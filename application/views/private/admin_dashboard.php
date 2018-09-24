6408d9acbfef5e5c6c5fe13483c23d99bcf5332e
Prema Waisnawa <31471303+premawaisnawa@users.noreply.github.com>
2018-09-24 14:05:08 +0800

    revisi

--- application/views/private/admin_dashboard.php
+++ application/views/private/admin_dashboard.php
@@ -81,8 +81,8 @@
       <div class="nav-tabs-custom">
             <!-- Tabs within a box -->
             <ul class="nav nav-tabs pull-right">
-              <li class="active"><a href="#sales-chart" data-toggle="tab">Donut</a></li>
-              <!-- <li class="pull-left header"><i class="fa fa-inbox"></i> Sales</li> -->
+              <!-- <li class="active"><a href="#sales-chart" data-toggle="tab">Donut</a></li> -->
+              <li class="pull-left header"><i class="fa fa-inbox"></i> Member</li>
             </ul>
             <div class="tab-content no-padding">
               <div class="chart tab-pane active" id="sales-chart" style="position: relative; height: 300px;"></div>
